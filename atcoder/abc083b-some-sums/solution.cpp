fe7181ceaef64f4f56f915425123d013e3361073
hardway <hardway120308@gmail.com>
2026-03-08 21:03:12 +0800

    solve AtCoder "Some Sums"

---
+++ atcoder/abc083b-some-sums/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <numeric>
+
+using namespace std;
+
+int digit_sum(int x) {
+    int sum = 0;
+    while (x > 0) {
+        sum += x % 10;
+        x /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int n, a, b;
+    vector<int> ans;
+    cin >> n >> a >> b;
+    for (int i = 1; i <= n; i++) {
+        if (a <= digit_sum(i) and digit_sum(i) <= b) {
+            ans.push_back(i);
+        }
+    }
+    cout << accumulate(ans.begin(), ans.end(), 0) << endl;
+    return 0;
+}
