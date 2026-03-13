1959d28d485d73027bbce788377362cbca5dc59b
hardway <hardway120308@gmail.com>
2026-03-13 19:15:18 +0800

    實作AWC Beta 0024 Task 4

--- atcoder/weekly-contest-beta-0023/solution4.cpp
+++ atcoder/weekly-contest-beta-0023/solution4.cpp
@@ -1,40 +1,63 @@
 #include <bits/stdc++.h>
+#include <utility>
 
 using namespace std;
 
-int wn[51];
-int pn[51];
-int n, s, t;
-int ans = -1;
-
-int dp(int n, int weight, int selected) {
-    if (n <= 0) {
-        return pn[0];
-    }
-    if (weight + wn[n] < s) {
-        int x = min(dp(n, weight - wn[n], selected + 1), dp(n - 1, weight, selected));
-        if (x >= t and selected <= ans) {
-            ans = selected;
-        }
-        return x;
-    } else {
-        return dp(n, weight, selected);
-    }
-}
-
 int main() {
+    const int NEG_INF = -1;
+    int n, s, t;
     cin >> n >> s >> t;
+
+    int ans = -1;
+    // dp表代表選擇了k個項目，總重量**精確**為w的最大利潤
+    int dp[n + 1][s + 1];
+    vector<pair<int, int>> items;
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= s; j++) {
+            dp[i][j] = NEG_INF;
+        }
+    }
+    dp[0][0] = 0;
 
     for (int i = 0; i < n; i++) {
         int p, c, w;
         cin >> p >> c >> w;
         int profit = p - c;
-        wn[i] = w;
-        pn[i] = profit;
+        if (profit > 0 and w <= s) {
+            items.push_back(make_pair(w, profit));
+        }
     }
 
-    cout << dp(n - 1, 0, 0) << endl;
+    for (auto item : items) {
+        int weight = item.first;
+        int val = item.second;
+        // 倒序避免重複選取
+        for (int i = n; i >= 1; i--) {
+            // 要運送此請求至少要w的重量，但最多不會超過S
+            for (int curr_weight = weight; curr_weight <= s; curr_weight++) {
+                // 如果可以透過選擇項目到達
+                if (dp[i - 1][curr_weight - weight] != NEG_INF) {
+                    // dp表更新為前一個項目中選取本項目或者保留目前的值不變
+                    // 要取和現在值max的原因是如果有同重量的物品，則第i=0迴圈進來之後會壓到同樣dp[1][W_1]的值
+                    // 此時需要判斷誰的利潤更大才是局部最優解
+                    dp[i][curr_weight] = max(dp[i][curr_weight], dp[i - 1][curr_weight - weight] + val);
+                }
+            }
+        }
+    }
 
-    cout << ans << endl;
+    for (int k = 0; k <= n; k++) {
+        for (int w = 0; w <= s; w++) {
+            if (dp[k][w] >= t) {
+                cout << k << endl;
+                return 0;
+            } else if (k == n and w == s) {
+                cout << -1 << endl;
+                return 0;
+            }
+        }
+    }
+
     return 0;
 }
